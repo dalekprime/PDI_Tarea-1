91c8b67ad441a5d1ac82a6e736f83bc59e56960f
dalekprime <bryansvzla@gmail.com>
2025-12-19 00:56:29 -0400

    Forma Final

--- Tarea1-27531458-BryanSilva-28309661-OrianaArellano/src/main/kotlin/actions/UmbralizerController.kt
+++ Tarea1-27531458-BryanSilva-28309661-OrianaArellano/src/main/kotlin/actions/UmbralizerController.kt
@@ -1,6 +1,7 @@
 package actions
 
 import models.ImageMatrix
+import models.Pixel
 
 class UmbralizerController {
     //Umbral Simple
@@ -10,15 +11,8 @@ class UmbralizerController {
         for (y in 0 until height) {
             for (x in 0 until width) {
                 val grey = (0.299*imageMatrix.pixels[y][x].r + 0.587*imageMatrix.pixels[y][x].g + 0.114*imageMatrix.pixels[y][x].b)
-                if( grey < threshold.toInt()) {
-                    imageMatrix.pixels[y][x].r = 0
-                    imageMatrix.pixels[y][x].g = 0
-                    imageMatrix.pixels[y][x].b = 0
-                }else{
-                    imageMatrix.pixels[y][x].r = 255
-                    imageMatrix.pixels[y][x].g = 255
-                    imageMatrix.pixels[y][x].b = 255
-                }
+                val ele = if (grey < threshold.toInt()) 0 else 255
+                imageMatrix.pixels[y][x] = Pixel(ele, ele, ele)
             }
         }
         return imageMatrix
@@ -27,17 +21,15 @@ class UmbralizerController {
     fun multiUmbral(imageMatrix: ImageMatrix, thresholdInf: Number, thresholdSup: Number): ImageMatrix {
         val width = imageMatrix.width
         val height = imageMatrix.height
+        val t1 = thresholdInf.toInt()
+        val t2 = thresholdSup.toInt()
         for (y in 0 until height) {
             for (x in 0 until width) {
                 val grey = (0.299*imageMatrix.pixels[y][x].r + 0.587*imageMatrix.pixels[y][x].g + 0.114*imageMatrix.pixels[y][x].b)
-                if(thresholdInf.toInt() > grey && grey < thresholdSup.toInt()){
-                    imageMatrix.pixels[y][x].r = 0
-                    imageMatrix.pixels[y][x].g = 0
-                    imageMatrix.pixels[y][x].b = 0
-                }else{
-                    imageMatrix.pixels[y][x].r = 255
-                    imageMatrix.pixels[y][x].g = 255
-                    imageMatrix.pixels[y][x].b = 255
+                if (grey >= t1 && grey <= t2) {
+                    imageMatrix.pixels[y][x] = Pixel(255, 255, 255)
+                } else {
+                    imageMatrix.pixels[y][x] = Pixel(0, 0, 0)
                 }
             }
         }
